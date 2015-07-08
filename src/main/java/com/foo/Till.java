5f820dc3cd40a786f34d472adc74a5409eacf7c6
stonefish258 <k5X8R2h7@gmail.com>
2015-07-08 19:02:32 +0100

    added test for basket containing single apple

--- src/main/java/com/foo/Till.java
+++ src/main/java/com/foo/Till.java
@@ -1,8 +1,32 @@
 package com.foo;
 
+import java.util.Arrays;
+
 public class Till {
 
-    public double calculateTotal(String[] emptyBasket) {
-        return 0;
+    private static final String APPLE = "Apple";
+    private static final String ORANGE = "Orange";
+
+    public double calculateTotal(String[] basket) {
+
+        double total = Arrays.stream(basket)
+                              .mapToDouble(s -> {
+
+                                  double result = 0;
+                                  if (s.equals(APPLE)) {
+                                      result = 0.60d;
+                                  } else
+                                  if (s.equals(ORANGE)) {
+                                      result = 0.25d;
+                                  } else {
+                                      // shouldn't happen
+                                      result = 0.0d;
+                                  }
+
+                                  return result;
+                              })
+                              .sum();
+
+        return total;
     }
 }
--- src/test/java/com/foo/TillTest.java
+++ src/test/java/com/foo/TillTest.java
@@ -1,19 +1,25 @@
 package com.foo;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class TillTest {
 
+    private Till till;
 
     private static final double DELTA = 0.009;
+
+    @Before
+    public void setup() {
+        till = new Till();
+    }
 
     @Test
     public void emptyBasketHasZeroTotalCost() {
 
         // given
-        Till till = new Till();
         String[] emptyBasket = {};
 
         // when
@@ -21,5 +27,18 @@ public class TillTest {
 
         // then
         assertEquals(0, total, DELTA);
+    }
+
+    @Test
+    public void basketContainingOneAppleShouldCostSixtyPence() {
+
+        // given
+        String[] basket = {"Apple"};
+
+        // when
+        double total = till.calculateTotal(basket);
+
+        // then
+        assertEquals(0.60d, total, DELTA);
     }
 }
